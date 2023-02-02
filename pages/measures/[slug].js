13021d9329d32e2e1f56c8c2a63622adb70aa5fd
Mohamed Salah <mohamedsalahsebai@gmail.com>
2023-02-02 11:34:57 +0200

    fixing animation glitch on mobile phone

--- pages/measures/[slug].js
+++ pages/measures/[slug].js
@@ -97,17 +97,11 @@ function Meausre({measure}) {
                     </main>
                 </div>
             </div>
-            <motion.div 
-                initial={{x: -20, y: -20}} 
-                animate={{x: 0, y: 0}} 
-                transition={{duration: 0.5}}
-            >
-                <Link href='/' className={styles.backButton}>
-                    <svg id="i-chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                        <path d="M12 30 L24 16 12 2" />
-                    </svg>
-                </Link>
-            </motion.div>
+            <Link href='/' className={styles.backButton}>
+                <svg id="i-chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <path d="M12 30 L24 16 12 2" />
+                </svg>
+            </Link>
         </motion.div>
 
     )
